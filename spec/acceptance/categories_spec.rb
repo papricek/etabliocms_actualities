932976fb33d8f3117933f347911eab494ea3047b
Patrik Jira <patrikjira@gmail.com>
2012-02-25 14:12:43 +0100

    Spec for categories

---
+++ spec/acceptance/categories_spec.rb
@@ -0,0 +1,63 @@
+# encoding: utf-8
+require 'acceptance/acceptance_helper'
+
+feature 'Categories', '' do
+
+  background do
+    user = FactoryGirl.create(:user)
+    login_as(user)
+  end
+
+  scenario 'Some crud for admin' do
+    click_link('Kategorie (0)')
+    page.should have_content('Kategorie')
+    click_link('Přidat kategorii »')
+    click_button('Uložit')
+    page.should have_content('Nadpis je povinná položka')
+    fill_in 'Nadpis', :with => 'A title with české znaky ěščřžýáíé'
+    click_button('Uložit')
+    page.should have_content('Kategorie byla úspěšně vytvořena.')
+    page = EtabliocmsActualities::Category.last
+    page.title.should == 'A title with české znaky ěščřžýáíé'
+    page.slug.should == 'a-title-with-ceske-znaky-escrzyaie'
+  end
+
+  scenario 'Moving categories in hierarchy via edit' do
+    root = FactoryGirl.create(:category)
+    first_child = FactoryGirl.create(:category, :child_of => root.id)
+    second_child = FactoryGirl.create(:category, :child_of => root.id)
+    first_child.parent.should == root
+    second_child.parent.should == root
+    visit admin_homepage
+    click_link('Kategorie (3)')
+    click_link(first_child.title)
+    page.should have_content(first_child.title)
+    select 'Nejvyšší úroveň', :from => 'Rodič'
+    click_button('Uložit')
+    page.should have_content('Kategorie byla úspěšně upravena.')
+    first_child.reload.parent.should be_nil
+    click_link(first_child.title)
+    select root.title, :from => 'Rodič'
+    click_button('Uložit')
+    page.should have_content('Kategorie byla úspěšně upravena.')
+    first_child.reload.parent.should == root
+  end
+
+  scenario 'Moving categories in hierarchy via arrows' do
+    root = FactoryGirl.create(:category)
+    first_child = FactoryGirl.create(:category, :child_of => root.id)
+    second_child = FactoryGirl.create(:category, :child_of => root.id)
+    root.children.first.should == first_child
+    visit admin_homepage
+    click_link('Kategorie (3)')
+    find(:xpath, "//a[@href='/admin/categories/#{first_child.id}/move?method=move_lower']").click
+    page.should have_content('Kategorie byla úspěšně přesunuta.')
+    root.children.reload.first.should == second_child
+    page.driver.put("/admin/categories/#{first_child.id}/move?method=move_to_bottom")
+    root.children.reload.first.should == second_child
+    find(:xpath, "//a[@href='/admin/categories/#{second_child.id}/move?method=move_to_bottom']").click
+    page.should have_content('Kategorie byla úspěšně přesunuta.')
+    root.children.reload.first.should == first_child
+  end
+
+end
---
+++ spec/factories.rb
@@ -0,0 +1,26 @@
+require 'ffaker'
+
+FactoryGirl.define do
+
+  sequence :title do |n|
+    Faker::Lorem.sentence(3)
+  end
+
+  sequence :email do |n|
+    name = [Faker::Name.first_name, Faker::Name.last_name].map { |i| i.gsub(/\W/, '') }.join(".").downcase
+    "#{name}#{n}@#{Faker::Internet.domain_name}"
+  end
+
+  factory :user, :class => 'EtabliocmsCore::User' do
+    email
+    password "123456"
+    password_confirmation "123456"
+  end
+
+  factory :category, :class => 'EtabliocmsActualities::Category' do
+    title
+    text { Faker::Lorem.paragraphs.to_s }
+    locale "cs"
+  end
+
+end
